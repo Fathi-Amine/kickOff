cf799d5e72c3116b38dfcad2d93a8f028547f842
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2024-02-20 11:27:49 +0100

    setting up project with expo and adding global styles file for the screens

---
+++ assets/css/GlobalStyles.js
@@ -0,0 +1,33 @@
+
+export const FontFamily = {
+    beVietnamProMedium: "BeVietnamPro-Medium",
+    beVietnamProRegular: "BeVietnamPro-Regular",
+    beVietnamProBold: "BeVietnamPro-Bold",
+};
+
+export const FontSize = {
+    size_xs: 12,
+    size_sm: 14,
+    size_base: 16,
+    size_lg: 18,
+};
+
+export const Color = {
+    colorWhite: "#fff",
+    colorGray_100: "#26241c",
+    colorGray_200: "#171712",
+    colorDarkslategray: "#3b3626",
+    colorDarkgray: "#bab39c",
+};
+
+export const Padding = {
+    p_base: 16,
+    p_5xs: 8,
+    p_xs: 12,
+    p_9xs: 4,
+};
+
+export const Border = {
+    br_5xs: 8,
+    br_xs: 12,
+};
